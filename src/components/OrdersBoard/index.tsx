353e25b4f2a7ccd44893eac87252b2c78afab68a
Abimael Lovera <abimaellovera@gmail.com>
2022-11-29 21:57:34 -0300

    Criando Board

---
+++ src/components/OrdersBoard/index.tsx
@@ -0,0 +1,25 @@
+import { Board, OrdersContainer } from './styles';
+
+export function OrdersBoard() {
+  return(
+    <Board>
+      <header>
+        <span>🕒️</span>
+        <strong>fila de espera</strong>
+        <span>(1)</span>
+      </header>
+
+      <OrdersContainer>
+        <button type="button">
+          <strong>Mesa 2</strong>
+          <span>2 itens</span>
+        </button>
+
+        <button type="button">
+          <strong>Mesa 2</strong>
+          <span>2 itens</span>
+        </button>
+      </OrdersContainer>
+    </Board>
+  );
+}
