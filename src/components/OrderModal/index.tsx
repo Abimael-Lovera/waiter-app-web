81d03db0efbd2a3dbb6a641e71382bc61c3d2aa2
Abimael Lovera <abimaellovera@gmail.com>
2022-11-30 22:39:08 -0300

    config intl api javascript

--- src/components/OrderModal/index.tsx
+++ src/components/OrderModal/index.tsx
@@ -1,6 +1,7 @@
 import { Overlay, ModalBody, OrderDetails } from './styles';
 import closeIcon from '../../assets/images/close-icon.svg';
 import { Order } from '../../types/Order';
+import { formatCurrency } from '../utils/formatCurrency';
 
 interface OrderModalProps {
   visible: boolean;
@@ -11,6 +12,7 @@ export function OrderModal({visible, order}: OrderModalProps) {
   if(!visible || !order) {
     return null;
   }
+
   return(
     <Overlay>
       <ModalBody>
@@ -40,16 +42,26 @@ export function OrderModal({visible, order}: OrderModalProps) {
 
         <OrderDetails>
           <strong>Itens</strong>
-          {order.products.map(({_id, product, quantity}) => (
-            <div className="item" key={_id}>
-              <img
-                src={`http://localhost:3001/uploads/${product.imagePath}`}
-                alt={product.name}
-                width="56"
-                height="28.51"
-              />
-            </div>
-          ))}
+
+          <div className="order-items">
+            {order.products.map(({_id, product, quantity}) => (
+              <div className="item" key={_id}>
+                <img
+                  src={`http://localhost:3001/uploads/${product.imagePath}`}
+                  alt={product.name}
+                  width="56"
+                  height="28.51"
+                />
+
+                <span className="quantity">{quantity}X</span>
+
+                <div className="product-details">
+                  <strong>{product.name}</strong>
+                  <span>{formatCurrency(product.price)}</span>
+                </div>
+              </div>
+            ))}
+          </div>
         </OrderDetails>
       </ModalBody>
     </Overlay>
